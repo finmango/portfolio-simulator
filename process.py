3e928a939c1c18f4cea2be15e556448a3739c84b
Oscar Wahltinez <41451293+owahltinez@users.noreply.github.com>
2022-01-13 11:28:29 -0500

    Remove charts

--- process.py
+++ process.py
@@ -1,7 +1,4 @@
 import pandas as pd
-from matplotlib import pyplot as plt
-import seaborn
-seaborn.set_context("notebook")
 
 # Read raw historical prices from CSV file
 P = pd.read_csv('data/prices.csv').set_index('Date')
@@ -18,14 +15,6 @@ LTTx3 = R['LTT'].apply(lambda x: x * 3 - 0.080 / 252 if x != 0 else 0)
 R['UPRO'] = R['UPRO'].fillna(SPYx3)
 R['TMF'] = R['TMF'].fillna(LTTx3)
 
-# Plot comparisons between leveraged and estimated assets
-(1 + R[['UPRO']].assign(SPYx3=SPYx3).dropna()).cumprod().plot(figsize=(16,4)).legend(loc='upper left')
-plt.tight_layout()
-plt.savefig('UPRO-estimate.svg')
-(1 + R[['TMF']].assign(LTTx3=LTTx3).dropna()).cumprod().plot(figsize=(16,4)).legend(loc='upper left')
-plt.tight_layout()
-plt.savefig('TMF-estimate.svg')
-
 # Load dividend data from CSV file
 div = {}
 for symbol, row in pd.read_csv('data/dividends.csv').set_index('Symbol').iterrows():
@@ -40,16 +29,6 @@ records = []
 for date, row in R.iterrows():
   records.append(dict(Date=date, **{k: v + div.get(symbol, {}).get(date, 0) for k, v in row.to_dict().items()}))
 D = pd.DataFrame.from_records(records).set_index('Date')
-
-# Plot comparisons between returns with and without dividend reinvestment
-for symbol in D.columns:
-  if symbol not in div: continue
-  X = pd.DataFrame(index=D.index)
-  X[symbol] = R[symbol]
-  X[f'{symbol} with dividends reinvested'] = D[symbol]
-  (1 + X.dropna()).cumprod().plot(figsize=(16,4)).legend(loc='upper left')
-  plt.tight_layout()
-  plt.savefig(f'{symbol}-dividends.svg')
 
 # Save data as CSV files
 R.dropna(how='all').to_csv('daily-returns.csv')
